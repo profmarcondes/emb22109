9ab9dd2f7b40a5aaf9c76b7f0c196edf54639379
Hugo Marcondes <hugo.marcondes@ifsc.edu.br>
2025-07-11 00:32:03 -0300

    minor updates on Container generation file

--- scripts/setup_container.sh
+++ scripts/setup_container.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-podman build --squash --tag locahost/emb22109-toolbox:latest Container/
-toolbox create -i locahost/emb22109-toolbox:latest emb22109
+podman build --squash --tag emb22109-toolbox:latest Container/
+toolbox create -i emb22109-toolbox:latest emb22109
