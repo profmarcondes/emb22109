9185543c63e4b356fe466bcb68dc754ffffad1cd
Hugo Marcondes <hugo.marcondes@ifsc.edu.br>
2025-09-10 09:27:54 -0300

    pins update

--- statecharts/semaphore/include/arduinoPins.h
+++ statecharts/semaphore/include/arduinoPins.h
@@ -7,8 +7,8 @@ const int button_2_pin = 8;    // (Ped request) the number of the pushbutton 2 p
 const int led_green_pin = 5;
 const int led_yellow_pin = 4;
 const int led_red_pin = 3;
-const int ped_led_red_pin = 6;
-const int ped_led_green_pin = 7;
+const int ped_led_red_pin = 7;
+const int ped_led_green_pin = 6;
 const int ped_led_request_pin = 13;
 
 #endif /* ARDUINOPINS_H_ */
